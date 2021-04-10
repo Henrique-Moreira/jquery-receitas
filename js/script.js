9e99aa1d6cac8ca83a8938739c9c76dfd1eee5fe
ElianMelo <elianmelomorais@gmail.com>
2021-04-10 14:38:03 -0300

    Começa geracao de receita com base json

--- js/script.js
+++ js/script.js
@@ -26,10 +26,53 @@ function hideAll() {
 function getReceitas() {
     let jsonFilePath = "json/receitas.json";
     $.get(jsonFilePath, function(data) {
-        fillBoxs(data);
+        createRecipes(data);
     });
 }
 
-function fillBoxs(receitas) {
-    console.log(receitas);
+function createRecipes(receitas) {
+    let recipes = $('#recipes');
+    receitas.forEach((el) => createRecipe(el, recipes));
+}
+
+function createRecipe(receita, recipes) {
+
+    /*
+                <div class="metade">
+                <h2>Torta de Bolacha</h2>
+                <img src="https://t2.rg.ltmcdn.com/pt/images/4/5/2/img_torta_de_bolacha_com_flan_e_chocolate_4254_600.jpg" alt="">
+            </div>
+            <div class="box">
+                <div class="metade">
+                    <h3>Ingredientes</h3>
+                    <ul>
+                        <li>2 copos leite</li>
+                    </ul>
+                </div>
+                <div class="metade">
+                    <h3>Modo de Preparo</h3>
+                    <ol>
+                        <li>Leve ao fogo</li>
+                    </ol>
+                </div>
+            </div>
+    */
+    
+    let h2 = $("<h2>");
+    h2.text(receita.titulo);
+
+    let img = $("<img>");
+    img.attr("src", receita.src);
+
+    let ingredientes = $("<ul>");
+    receita.ingredientes.forEach((el) => ingredientes.append( $("<li>").text(el) ) );
+
+    let modo_preparo = $("<ol>");
+    receita.modo_preparo.forEach((el) => modo_preparo.append( $("<li>").text(el) ) );
+
+    recipes.append(h2);
+    recipes.append(img);
+    recipes.append(ingredientes);
+    recipes.append(modo_preparo);
+
 }
